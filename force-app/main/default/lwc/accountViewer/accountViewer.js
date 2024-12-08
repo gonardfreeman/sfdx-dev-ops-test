eda20d9d8695ae7ac71ca1fce81134822562698c
Dmitry Bondarenko <dmitry.bondarenko@silverlinecrm.com>
2024-12-08 16:35:53 -0600

    added small changes

--- force-app/main/default/lwc/accountViewer/accountViewer.js
+++ force-app/main/default/lwc/accountViewer/accountViewer.js
@@ -2,16 +2,16 @@ import { LightningElement, wire } from "lwc";
 import getAccounts from "@salesforce/apex/AccountViewerController.getAccounts";
 
 class AccountViewer extends LightningElement {
-  offset = 0;
+	offset = 0;
 
-  @wire(getAccounts, { offset: "$offset" }) wiredAccounts;
+	@wire(getAccounts, { offset: "$offset" }) wiredAccounts;
 
-  get accountsToShow() {
-    if (!Array.isArray(this.wiredAccounts.data)) {
-      return [];
-    }
-    return this.wiredAccounts.data;
-  }
+	get accountsToShow() {
+		if (!Array.isArray(this.wiredAccounts.data)) {
+			return [];
+		}
+		return this.wiredAccounts.data;
+	}
 }
 
 export default AccountViewer;
